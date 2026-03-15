d943c9221aa377cde4a8e5acf1c9a4f995d2f5e1
Ravindra Vidyasagar Pandey <ravindra.pandey127825@marwadiuniversity.ac.in>
2026-03-15 16:47:26 +0530

    remaster ui2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import "./app.css"   // ← add this line
+import "./App.css"   // ← add this line
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
